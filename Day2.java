cddd9557ba86fa7130ddbb3d5c82417ea9523927
Sam Dev <135120558+Weaponised115@users.noreply.github.com>
2024-12-04 11:57:51 +0000

    Update Day2.java

--- Day2.java
+++ Day2.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class Day2 {
 
 	public static void main(String[] args) throws FileNotFoundException {
-		File file = new File("file2.txt");
+		File file = new File("C:/Users/The Wraith/eclipse-workspace/AOCDay2/src/file2.txt");
 		Scanner scanner = new Scanner(file);
 		StringBuilder str = new StringBuilder();
 		ArrayList<int[]> List = new ArrayList<int[]>();
@@ -30,51 +30,51 @@ public class Day2 {
 			
 			for(int j = 0; j < tempArray.length; j++) {
 				
-				System.out.println("Reports I J: " + reports[i][j]);
-				System.out.println("Temp J: " + tempArray[j]);
-				
 				reports[i][j] = Integer.parseInt(tempArray[j]);
 			}
 		}
 		
-		System.out.println(reports[1][1]);
 		
-		String[] reportStr = new String[reports.length];
+		for (int i =0; i<reports.length; i++) {
+			List.add(reports[i]);
+		}
 		
-//		
-//		for(int i = 0; i<reports.length; i++) {
-//			int tempLine = reports[i];
-//			reportStr[i] = String.valueOf(tempLine);
-//			String[] reportFinal = reportStr[i].split("");
-//			int[] reportFinalInt = new int[reportFinal.length];
-//				
-//				for(int y = 0;y < reportFinal.length;y++){
-//					reportFinalInt[y] = Integer.parseInt(reportFinal[y]);
-//				}
-//				List.add(reportFinalInt);
-//		}
-//		
+		
+
 		int total = 0;
-		
-		for(int i =0; i<List.size() - 1; i++) {
-			for(int j=0; j<List.get(i).length - 1; j++){
-				int diff = List.get(i)[j] - List.get(i)[j+1];
-				if(List.get(i)[j] > List.get(i)[j+1] && diff >=1 && diff <=3) {
-					total += 1;
-					continue;
-				} else if(List.get(i)[j] < List.get(i)[j+1]  && diff >=1 && diff <=3) {
-					total += 1;
-					continue;
-				} else {
-					System.out.println("List " + Arrays.toString(List.get(i)) + " is wrong");
-					total -= 1;
+
+		for(int i =0; i<reports.length; i++) {
+			
+			boolean reportIncreasing = true;
+			boolean noUnsafesFound = true;
+			
+			for(int j=0; j< reports[i].length - 1; j++){
+				int diff = Math.abs(reports[i][j] - reports[i][j+1]);
+				if(reports[i][j] > reports[i][j+1] && diff >=1 && diff <=3) {
+					if(j == 0){
+						reportIncreasing = true;
+					}
+					if(j > 0 && !reportIncreasing){
+						noUnsafesFound = false;
+					}
+					
+					} else if(reports[i][j] < reports[i][j+1] && diff >=1 && diff <=3) {
+						
+						if(j == 0){
+							reportIncreasing = false;
+						}
+						if(j > 0 && reportIncreasing){
+							noUnsafesFound = false;
+					}
+					} else {
+						noUnsafesFound = false;
+						System.out.println("List " + Arrays.toString(reports[i]) + " is wrong");
+					}
 				}
+			if(noUnsafesFound){
+				total += 1;
 			}
 		}
 		System.out.println(total);
 	}
 }
-
-//		Loop through
-//		turn array to a string
-//		split back into its own array
